046af5bc4c1aaf98b2ee6971f25982e0d4d62df8
hgSilv <39529919+hgSilv@users.noreply.github.com>
2018-06-07 12:47:14 -0500

    incompleto

---
+++ string.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : string.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "string.h"
+
+struct strString{
+	char *chars;
+	unsigned int length;
+};
